882887bb25680d752376f4113daed0e4180a16e9
builder <customersuccess@zivid.com>
2022-06-17 09:58:41 +0000

    Update HDevelop samples to SDK 2.6.1

---
+++ source/SampleUtils/yaml_settings_to_json.py
@@ -0,0 +1,31 @@
+""" Script to convert from .yaml/.yml to .json
+"""
+
+import argparse
+import json
+from pathlib import Path
+
+import yaml
+
+
+def _args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", type=Path, required=True)
+    return parser.parse_args()
+
+
+def _main() -> None:
+    input_directory = _args().directory
+    if not input_directory.is_dir():
+        raise Exception(f"{input_directory} is not a directory")
+    output_directory = Path(f"{input_directory}_json")
+    output_directory.mkdir(exist_ok=True)
+    for x in list(_args().directory.glob("*.yml")):
+        output_file = output_directory / f"{x.stem}.json"
+        with open(x, "r", encoding="utf-8") as yaml_in, open(output_file, "w", encoding="utf-8") as json_out:
+            yaml_object = yaml.safe_load(yaml_in)
+            json.dump(yaml_object, json_out, indent=2)
+
+
+if __name__ == "__main__":
+    _main()
