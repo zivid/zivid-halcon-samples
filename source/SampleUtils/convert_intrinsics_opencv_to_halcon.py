1921e92283ac45e3f97e2fe7697023b63e87d541
builder <customersuccess@zivid.com>
2026-06-25 14:31:02 +0000

    Samples: Link samples to their tutorials and articles
    Add links from the samples to the relevant Knowledge Base
    tutorials and articles, so each sample points to where to read
    more.
    
    Links added to sample headers:
    - Capture, CaptureHDRCompleteSettings,
      CaptureWithSettingsFromYML: link to the capture tutorial and
      camera settings article.
    - CaptureSavePLY and ReadPLY: link to the point cloud structure
      and output formats article (ReadPLY's existing link updated to
      its current location).
    - Warmup: link to the camera warm-up tutorial.
    - EyeInHandCalibration3DObject and
      EyeInHandCalibrationMVTecPlate: link to the Hand-Eye
      calibration in HALCON tutorial.
    - convert_intrinsics_opencv_to_halcon: link to the camera
      intrinsics article.
    
    README:
    - Add a "Capture Settings in HALCON" entry to the list of
      tutorials.

--- source/SampleUtils/convert_intrinsics_opencv_to_halcon.py
+++ source/SampleUtils/convert_intrinsics_opencv_to_halcon.py
@@ -25,6 +25,9 @@ example YML file for this sample can be found under the main instructions for Zi
 
 Note: This example uses experimental SDK features, which may be modified, moved, or deleted in the future without notice.
 
+For more information on camera intrinsics, check out this article:
+https://support.zivid.com/en/latest/camera/reference-articles/camera-intrinsics.html
+
 """
 
 import argparse
